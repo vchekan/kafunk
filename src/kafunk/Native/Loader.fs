ae708ec54b184afbd8b82449ace051f4508aa43c
Vadim Chekan <kot.begemot@gmail.com>
2018-04-13 22:22:25 -0400

    fix

--- src/kafunk/Native/Loader.fs
+++ src/kafunk/Native/Loader.fs
@@ -52,7 +52,6 @@ module Loader =
                 // https://github.com/dotnet/corefx/issues/19694
                 // On top of this, RuntimeInformation is supported in NET471+ and not NET45 which we target
                 #if NET45
-                OperatingSystem.
                 loadUnix (sprintf "lib/linux64-libc6/%s" name)
                 #else
                 if RuntimeInformation.IsOSPlatform(OSPlatform.OSX) then
