0e571535eb0bd283a66344d2278053882cc6008c
Vadim Chekan <kot.begemot@gmail.com>
2018-04-13 21:19:09 -0400

    Compilation fix

--- src/kafunk/Native/Loader.fs
+++ src/kafunk/Native/Loader.fs
@@ -49,6 +49,7 @@ module Loader =
                 // https://github.com/dotnet/corefx/issues/19694
                 // On top of this, RuntimeInformation is upported in NET471+ and not NET45 which we target
                 #if NET45
+                ()
                 #else
                 if RuntimeInformation.IsOSPlatform(OSPlatform.OSX) then
                     loadUnix (sprintf "lib/OSX-10.12/%s" name)
